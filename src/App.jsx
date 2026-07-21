c4cc95b8cd65bb594d34fad750ef60d01ce47f37
Mohd Shoaib <shoaibmail30@gmail.com>
2026-07-21 23:56:33 +0530

    added functionality to add task & show them in 'your tasks' without database(volatile)

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,70 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  const handleEdit = () => {
+    
+  }
+  const handleDelete = () => {
+    
+  }
+  const handleSave = () => {
+    setTasks([...tasks,{id: uuidv4(), task, isCompleted: false}])
+    setTask("")
+  }
+  const handleChange = (e) => {
+    setTask(e.target.value)
+  }
+  const handleCheckbox = (e) => {
+    let id = e.target.name
+    let index = tasks.findIndex(item=>{
+      return item.id === id
+    })
+    let newTasks = [...tasks]
+    newTasks[index].isCompleted = !newTasks[index].isCompleted
+    setTasks(newTasks)
+  }
 
   return (
     <>
       <Navbar />
-      <div className="main mx-auto my-3 p-5 rounded-2xl bg-blue-100 w-1/2">
+      <div className="main mx-auto my-4 p-5 rounded-2xl bg-blue-100 w-1/2 shadow-lg shadow-blue-200">
 
-        <h2 className='font-bold text-lg text-gray-800'>Add a Task</h2>
+        {/* Adding the tasks */}
+        <h2 className='font-bold text-lg text-blue-500'>Add a Task</h2>
 
         <div className="addTask m-3">
-          <input type="text" className='bg-white w-1/2 mx-2 p-1 px-3 rounded-2xl placeholder-gray-400 ' placeholder="Enter today's task?"/>
-          <button className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Save</button>
+          {/* Input of Task */}
+          <input onChange={handleChange} value={task} type="text" className='bg-white w-1/2 mx-2 p-1 px-3 rounded-2xl placeholder-gray-400 ' placeholder="Enter today's task?"/>
+          <button onClick={handleSave} className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Save</button>
         </div>
 
-        <h2 className='font-bold text-lg text-gray-800'>Your Tasks</h2>
+        {/* Your saved tasks */}
+        <h2 className='font-bold text-lg text-blue-500'>Your Tasks</h2>
 
         <div className="tasks m-3 rounded-2xl bg-white">
-          <div className='px-3 py-1 task flex items-center justify-between border-b-2 border-blue-100'>
-            <div className="text"></div>
+          {tasks.map(item=>{
+          
+          return (
+          <div key={item.id} className='task px-3 py-1 flex items-center justify-between border-b-2 border-blue-100'>
+            {/* Checkbox */}
+            <input name={item.id} type="checkbox" onChange={handleCheckbox} value={item.isCompleted} />
+            {/* Text of Task */}
+            <div className={item.isCompleted?"line-through":""}>{item.task}</div>
+
+            {/* buttons to edit or delete tasks */}
             <div className="buttons">
-            <button className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Edit</button>
-            <button className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Delete</button>
+            <button onClick={handleEdit} className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Edit</button>
+            <button onClick={handleDelete} className='m-1 px-3 py-1 font-bold text-white text-sm bg-blue-500 rounded-2xl hover:bg-blue-600 transition-all cursor-pointer'>Delete</button>
             </div>
           </div>
+          )
+          })}
         </div>
 
       </div>
